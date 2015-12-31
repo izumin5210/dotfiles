6d2706511b98b5665331b306706bd98577a6e936
izumin5210 <masayuki@izumin.info>
2015-12-31 18:33:02 +0900

    Merge fetch.sh into install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,35 @@
 set -eu
 
 DOTPATH=~/.dotfiles
+GITHUB_URL="https://github.com/izumin5210/dotfiles"
 
-DOTPATH=$DOTPATH ./scripts/fetch.sh
+function has() {
+    return `type $1 > /dev/null 2>&1`
+}
+
+if has "git"; then
+    git clone --recursive "$GITHUB_URL" "$DOTPATH"
+
+elif has "curl" || has "wget"; then
+    tarball="$GITHUB_URL/archive/master.tar.gz"
+
+    if has "curl"; then
+        curl -L "$tarball"
+
+    elif has "wget"; then
+        wget -O - "$tarball"
+
+    fi | tar xv -
+
+    mv -f dotfiles-master "$DOTPATH"
+
+else
+    die "curl or wget required"
+fi
+
+cd $DOPATH
+if [ $? -ne 0 ]; then
+    die "not found: $DOTPATH"
+fi
+
 DOTPATH=$DOTPATH ./scripts/deploy.sh

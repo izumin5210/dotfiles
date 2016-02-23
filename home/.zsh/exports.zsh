f3f4d4aedc69f030a6366ebe18f4278be7aba485
izumin5210 <masayuki@izumin.info>
2016-02-23 14:54:15 +0900

    Set DYLD_LIBRARY_PATH for OpenNI2

--- home/.zsh/exports.zsh
+++ home/.zsh/exports.zsh
@@ -64,3 +64,6 @@ export HOMEBREW_CASK_OPTS="--appdir=/Applications --caskroom=/opt/homebrew-cask/
 export DOCKER_HOST=tcp://192.168.59.103:2376
 export DOCKER_CERT_PATH=/Users/izumin/.boot2docker/certs/boot2docker-vm
 export DOCKER_TLS_VERIFY=1
+
+# OpenNI2
+export DYLD_LIBRARY_PATH=/usr/local/lib/ni2:$DYLD_LIBRARY_PATH

8bdd55e611cf92aa488da0fe85e977f1f9017272
izumin5210 <masayuki@izumin.info>
2017-06-24 02:56:17 +0900

    Install apps from appstore

--- cookbooks/macos_apps/default.rb
+++ cookbooks/macos_apps/default.rb
@@ -25,3 +25,29 @@ cask 'skype'
 cask 'the-unarchiver'
 cask 'visual-studio-code'
 cask 'zeplin'
+
+package 'mas'
+
+define :mas, id: nil do
+  execute "Install #{params[:name]}" do
+    command "mas install #{params[:id]}"
+    not_if "mas list | grep -E '^#{params[:id]} '"
+  end
+end
+
+execute 'mas signin' do
+  not_if 'mas account'
+end
+
+[
+  { name: 'Display Menu', id: 549083868 },
+  { name: 'Keynote', id: 409183694 },
+  { name: 'PopClip', id: 445189367 },
+  { name: 'Slack', id: 803453959 },
+  { name: 'Wantedly Chat', id: 1076860635 },
+  { name: 'Xcode', id: 497799835 },
+].each do |app|
+  mas app[:name] do
+    id app[:id]
+  end
+end

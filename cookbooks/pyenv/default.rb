58fb4ee0bd18c6b369867c448e303d5229aec8b5
izumin5210 <masayuki@izumin.info>
2017-06-23 22:28:26 +0900

    Itamaeify pyenv

---
+++ cookbooks/pyenv/default.rb
@@ -0,0 +1,11 @@
+pyenv_root = "#{anyenv_root}/envs/pyenv"
+
+install_env 'pyenv'
+
+install_env_versions 'pyenv' do
+  versions node.dig(:pyenv, :versions)
+end
+
+env_global 'pyenv' do
+  version node.dig(:pyenv, :global)
+end

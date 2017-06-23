4cbff1e472f8e260b782663e6540ae7c2b7588f9
izumin5210 <masayuki@izumin.info>
2017-06-23 21:24:09 +0900

    Extract resources for anyenv

--- cookbooks/rbenv/default.rb
+++ cookbooks/rbenv/default.rb
@@ -1,38 +1,20 @@
 rbenv_root = "#{anyenv_root}/envs/rbenv"
 
-execute 'Install rbenv' do
-  command with_anyenv("yes | anyenv install rbenv")
-  not_if "test -e #{rbenv_root}"
-end
+install_env 'rbenv'
 
 git "#{rbenv_root}/plugins/rbenv-default-gems" do
   repository 'https://github.com/sstephenson/rbenv-default-gems'
   revision 'de4ff2e101c9221dcd92ed91a41edcea2be41945'
 end
 
-default_gems = node.dig(:rbenv, :'default-gems')
-if default_gems.is_a? Array
-  file "#{rbenv_root}/default-gems" do
-    content default_gems.join("\n")
-  end
+file "#{rbenv_root}/default-gems" do
+  content "#{(node.dig(:rbenv, :'default-gems') || []).join("\n")}\n"
 end
 
-versions = node.dig(:rbenv, :versions)
-if versions.is_a? Array
-  versions.each do |version|
-    cmd = "rbenv install #{version}"
-    execute cmd do
-      command with_anyenv(cmd)
-      not_if with_anyenv("rbenv versions | grep '#{version}'")
-    end
-  end
+install_env_versions 'rbenv' do
+  versions node.dig(:rbenv, :versions)
 end
 
-global = node.dig(:rbenv, :global)
-if global.is_a? String
-  cmd = "rbenv global #{global}"
-  execute cmd do
-    command with_anyenv(cmd)
-    not_if with_anyenv("rbenv global | grep '#{global}'")
-  end
+env_global 'rbenv' do
+  version node.dig(:rbenv, :global)
 end

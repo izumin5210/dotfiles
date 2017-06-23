80643f9ff25dc4a324307e0b5629dacba1717ae2
izumin5210 <masayuki@izumin.info>
2017-06-24 01:41:15 +0900

    Itamaeify fonts (including ricty)

---
+++ cookbooks/fonts/default.rb
@@ -0,0 +1,23 @@
+if node[:platform] == 'darwin'
+  brew_tap 'caskroom/fonts'
+
+  cask 'font-fontawesome'
+  cask 'font-inconsolata'
+  cask 'font-m-plus'
+  cask 'font-noto-sans-cjk-jp'
+  cask 'font-raleway'
+  cask 'font-raleway-dots'
+
+  brew_tap 'sanemat/font'
+  package 'ricty' do
+    options '--with-powerline'
+  end
+  execute 'fc-cache -vf' do
+    subscribes :run, 'execute[Copy ricty fonts]'
+    action :nothing
+  end
+  execute 'Copy ricty fonts' do
+    command 'cp -f /usr/local/opt/ricty/share/fonts/Ricty*.ttf ~/Library/Fonts/'
+    not_if 'ls -1 ~/Library/Fonts/ | grep -c -E "Ricty.*\.ttf$" | grep -E "^8$"'
+  end
+end

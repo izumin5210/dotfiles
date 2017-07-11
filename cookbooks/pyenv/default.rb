e7368a003c16ee38828e424337590933fe96ec11
izumin5210 <masayuki@izumin.info>
2017-07-12 00:03:43 +0900

    Introduce pyenv-default-packages

--- cookbooks/pyenv/default.rb
+++ cookbooks/pyenv/default.rb
@@ -4,6 +4,15 @@ pyenv_root = "#{anyenv_root}/envs/pyenv"
 
 install_env 'pyenv'
 
+git "#{pyenv_root}/plugins/pyenv-default-packages" do
+  repository 'https://github.com/jawshooah/pyenv-default-packages'
+  revision 'fb6f71a0288b5aec5723b6118aa0ca02e2cc9a23'
+end
+
+file "#{pyenv_root}/default-packages" do
+  content "#{(node.dig(:pyenv, :'default-packages') || []).join("\n")}\n"
+end
+
 install_env_versions 'pyenv' do
   versions node.dig(:pyenv, :versions)
 end

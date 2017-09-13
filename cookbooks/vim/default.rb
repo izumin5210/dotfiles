efbdeb0461695baa0cccc452dc97bb0aaee95911
izumin5210 <masayuki@izumin.info>
2017-09-13 23:39:42 +0900

    Use neovim HEAD

--- cookbooks/vim/default.rb
+++ cookbooks/vim/default.rb
@@ -7,7 +7,9 @@ if node[:platform] == 'darwin'
     options '--with-lua --with-python3' if node[:platform] == 'darwin'
   end
 
-  package 'neovim/neovim/neovim'
+  package 'neovim/neovim/neovim' do
+    options '--HEAD'
+  end
   cask "vimr"
 end
 

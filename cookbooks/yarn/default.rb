4a2a9a7f4db6298e9730be30729eb6e43ece89b1
izumin5210 <m@izum.in>
2020-01-15 21:58:43 +0900

    Integrate yarn cookbook into node

--- cookbooks/yarn/default.rb
+++
@@ -1,7 +0,0 @@
-yarn_version = node[:yarn][:version]
-yarn_root = "#{ENV['HOME']}/.yarn"
-
-execute 'Install yarn' do
-  command "nodenv init; curl -o- -L https://yarnpkg.com/install.sh | bash -s -- --version #{yarn_version}"
-  not_if "test -e #{yarn_root} && #{yarn_root}/bin/yarn --version | grep #{yarn_version}"
-end

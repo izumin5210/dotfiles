5a628f8a11d630c75b9ec14a42671ee28dd6fc0f
izumin5210 <m@izum.in>
2020-11-23 19:55:43 +0900

    Fix for darwin

--- cookbooks/git/default.rb
+++ cookbooks/git/default.rb
@@ -4,15 +4,19 @@ package 'git' do
   end
 end
 
-github_release 'gh' do
-  v = "1.2.1"
-  name = "gh_#{v}_#{node[:kernel][:name].downcase}_amd64"
+if node[:platform] == 'darwin'
+  package 'gh'
+else
+  github_release 'gh' do
+    v = "1.2.1"
+    name = "gh_#{v}_#{node[:kernel][:name].downcase}_amd64"
 
-  repo 'cli/cli'
-  version "v#{v}"
-  archive "#{name}.tar.gz"
-  checksum "gh_#{v}_checksums.txt"
-  bin "#{name}/bin/gh"
+    repo 'cli/cli'
+    version "v#{v}"
+    archive "#{name}.tar.gz"
+    checksum "gh_#{v}_checksums.txt"
+    bin "#{name}/bin/gh"
+  end
 end
 
 if node[:platform] == 'darwin'

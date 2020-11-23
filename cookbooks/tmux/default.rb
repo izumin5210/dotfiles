4d6d9b2b30ecdf0fed005b6d1abd357b9d014fd4
izumin5210 <m@izum.in>
2020-11-23 19:55:55 +0900

    Set user

--- cookbooks/tmux/default.rb
+++ cookbooks/tmux/default.rb
@@ -6,6 +6,7 @@ plugins_dir = "#{node[:home]}/.tmux/plugins"
 git "#{plugins_dir}/tpm" do
   repository 'https://github.com/tmux-plugins/tpm'
   revision 'c8ac32a085d382c43190bda4fb5972e531f501fd'
+  user node[:user]
 end
 
 execute "#{plugins_dir}/tpm/bin/install_plugins" do

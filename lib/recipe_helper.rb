b065fae45fe3a51cc6fea042ca0ea33e84b66fe1
izumin5210 <m@izum.in>
2021-12-03 12:55:58 +0900

    Move helpers

--- lib/recipe_helper.rb
+++ lib/recipe_helper.rb
@@ -45,4 +45,45 @@ MItamae::RecipeContext.class_eval do
 
     node.reverse_merge!(content)
   end
+
+  def type_from_value value
+    case value
+    when String      then 'string'
+    # when ???       then 'data'
+    when Integer     then 'int'
+    when Float       then 'float'
+    when TrueClass   then 'bool'
+    when FalseClass  then 'bool'
+    # when ???       then 'date'
+    when Array       then 'array'
+    # when ???       then 'array-add'
+    # when ???       then 'dict'
+    # when ???       then 'dict-add'
+    else
+      fail "Unexpected type: #{value} (#{value.class})"
+    end
+  end
+
+  def execute_macos_defaults(defaults, domain = 'NSGlobalDomain')
+    key = defaults[:key]
+    value = defaults[:value]
+    type = defaults[:type] || type_from_value(value)
+    execute "defaults write #{domain} #{key} -#{type} #{value}" do
+      not_if "defaults read #{domain} #{key} | grep -q -E '^#{value}$'"
+    end
+  end
+
+  def defaults_from_hash defaults_by_domain
+    defaults_by_domain.each do |domain, defaults_list|
+      defaults_list.each do |defaults|
+        execute_macos_defaults(defaults, domain)
+      end
+    end
+  end
+
+  def defaults_from_array defaults_list
+    defaults_list.each do |defaults|
+      execute_macos_defaults(defaults)
+    end
+  end
 end

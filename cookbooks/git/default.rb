2e29530c023461d0103abd49cac5df4d90d12786
izumin5210 <m@izum.in>
2021-12-06 11:35:16 +0900

    Support `/opt/homebrew`

--- cookbooks/git/default.rb
+++ cookbooks/git/default.rb
@@ -16,8 +16,8 @@ else
 end
 
 if node[:platform] == 'darwin'
-  link "/usr/local/bin/diff-highlight" do
-    to "/usr/local/share/git-core/contrib/diff-highlight/diff-highlight"
+  link "#{brew_prefix}/bin/diff-highlight" do
+    to "#{brew_prefix}/share/git-core/contrib/diff-highlight/diff-highlight"
   end
 end
 

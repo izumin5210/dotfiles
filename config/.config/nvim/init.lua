6155c82fbfb2650a71171cbc1ce1abeec792c93f
izumin5210 <m@izum.in>
2023-01-18 16:32:52 +0900

    tweak telescope keymap

--- config/.config/nvim/init.lua
+++ config/.config/nvim/init.lua
@@ -501,9 +501,8 @@ require('telescope').setup({
 })
 local telescope_builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader><leader>', telescope_builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', telescope_builtin.live_grep, {})
+vim.keymap.set('n', '<leader>g', telescope_builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fs', telescope_builtin.git_status, {})
-vim.keymap.set('n', '<leader>fb', telescope_builtin.buffers, {})
 vim.keymap.set('n', '<leader>fb', telescope_builtin.buffers, {})
 
 

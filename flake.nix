0eef2ccebe0c6d2af986cd3e724318ef37206b1f
izumin5210 <m@izum.in>
2024-03-17 00:31:34 +0900

    support different user names

--- flake.nix
+++ flake.nix
@@ -4,47 +4,48 @@
   inputs = {
     # Specify the source of Home Manager and Nixpkgs.
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     nix-darwin = {
       url = "github:LnL7/nix-darwin";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = inputs@{ nixpkgs, home-manager, nix-darwin, ... }:
+  outputs = inputs@{ self, nixpkgs, home-manager, nix-darwin, ... }:
     let
       system = "aarch64-darwin";
-      username = "masayuki.izumi";
-      hostname = "CM2NX3M6CH";
       pkgs = nixpkgs.legacyPackages.${system};
+
+      users = [
+        { username = "izumin"; homeDir = "/Users/izumin"; hostname = "fleur"; }
+        { username = "masayuki.izumi"; homeDir = "/Users/masayuki.izumi"; hostname = "CM2NX3M6CH"; }
+      ];
+
+      genHomeConfigurations = usersList: builtins.foldl' (acc: user:
+        acc // {
+          "${user.username}" = home-manager.lib.homeManagerConfiguration {
+            inherit pkgs;
+            modules = [
+              { home.username = user.username; home.homeDirectory = user.homeDir; }
+              ./home.nix
+            ];
+          };
+        }
+      ) { } usersList;
+
+      genDarwinConfigurations = usersList: builtins.foldl' (acc: user:
+        acc // {
+          "${user.hostname}" = nix-darwin.lib.darwinSystem {
+            inherit pkgs;
+            modules = [ ./darwin.nix ];
+          };
+        }
+      ) { } usersList;
     in {
-      darwinConfigurations = {
-        ${hostname} = nix-darwin.lib.darwinSystem {
-          # system = "aarch64-darwin";
-          system = system;
-          inherit pkgs;
-          modules = [
-            ./darwin.nix
-              home-manager.nixosModules.home-manager
-            # ({ pkgs, lib, ... }: {
-            #  home-manager.useGlobalPkgs = true;
-            #  home-manager.useUserPackages = true;
-            #  home-manager.users."masayuki.izumi" = { ... }: {
-            #  imports = [ ./home.nix ];
-            #  };
-            #  })
-            home-manager.darwinModules.home-manager
-            {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              users.users."masayuki.izumi".home = "/Users/${username}";
-              home-manager.users."masayuki.izumi" = import ./home.nix;
-            }
-          ];
-        };
-      };
+      homeConfigurations = genHomeConfigurations users;
+      darwinConfigurations = genDarwinConfigurations users;
     };
 }
--- home.nix
+++ home.nix
@@ -3,8 +3,8 @@
 {
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
-  home.username = "masayuki.izumi";
-  home.homeDirectory = "/Users/masayuki.izumi";
+  # home.username = user.name;
+  # home.homeDirectory = user.homeDir;
 
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release

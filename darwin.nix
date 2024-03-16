c5931f882358b08c5bfcc110365e39358a92651b
izumin5210 <m@izum.in>
2024-03-17 00:42:58 +0900

    update system.defaults

--- darwin.nix
+++ darwin.nix
@@ -2,7 +2,21 @@
 
 {
   system.defaults = {
-    dock.autohide = true;
+    NSGlobalDomain = {
+      KeyRepeat = 1;
+      InitialKeyRepeat = 10;
+      "com.apple.trackpad.scaling" = 2.0;
+    };
+    dock = {
+      autohide = true;
+      orientation = "right";
+    };
+    screensaver = {
+      askForPassword = true;
+    };
+    screencapture = {
+      location = "~/Downloads";
+    };
   };
 
   services.nix-daemon.enable = true;

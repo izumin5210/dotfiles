3f32b5f0868d2489a531dcc9acb1680c8aeb0b8a
Masayuki Izumi <m@izum.in>
2024-05-19 18:34:41 +0900

    chore(neovim): add `flash.nvim`, remove `flit.nvim` and `leap.nvim` (#401)

--- config/.config/nvim/init.lua
+++ config/.config/nvim/init.lua
@@ -484,19 +484,24 @@ require('lazy').setup({
     end,
   },
   {
-    'ggandor/flit.nvim',
-    dependencies = {
-      { 'ggandor/leap.nvim', lazy = true },
-    },
+    "folke/flash.nvim",
+    version = '*',
+    ---@type Flash.Config
+    opts = {},
+    -- stylua: ignore
     keys = {
-      { 'f', mode = { 'n' } },
-      { 'F', mode = { 'n' } },
-      { 't', mode = { 'n' } },
-      { 'T', mode = { 'n' } },
+      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+      { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+      { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+      { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+      { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+      { "f", mode = { "n" } },
+      { "F", mode = { "n" } },
+      { "t", mode = { "n" } },
+      { "T", mode = { "n" } },
+      { ",", mode = { "n" } },
+      { ";", mode = { "n" } },
     },
-    config = function()
-      require('flit').setup()
-    end
   },
   {
     'numToStr/Comment.nvim',

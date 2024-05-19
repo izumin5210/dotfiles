63e3ec5e8e3f08252db090ee61514bd1b7e31f5e
izumin5210 <m@izum.in>
2024-05-19 18:35:20 +0900

    feat(neovim): add neoscroll.nvim

--- config/.config/nvim/init.lua
+++ config/.config/nvim/init.lua
@@ -446,6 +446,23 @@ require('lazy').setup({
       })
     end
   },
+  {
+    "karb94/neoscroll.nvim",
+    config = function ()
+      require('neoscroll').setup({})
+      local t = {}
+      t['<C-u>'] = {'scroll', {'-vim.wo.scroll', 'true', '150'}}
+      t['<C-d>'] = {'scroll', { 'vim.wo.scroll', 'true', '150'}}
+      t['<C-b>'] = {'scroll', {'-vim.api.nvim_win_get_height(0)', 'true', '300'}}
+      t['<C-f>'] = {'scroll', { 'vim.api.nvim_win_get_height(0)', 'true', '300'}}
+      t['<C-y>'] = {'scroll', {'-0.10', 'false', '100'}}
+      t['<C-e>'] = {'scroll', { '0.10', 'false', '100'}}
+      t['zt']    = {'zt', {'250'}}
+      t['zz']    = {'zz', {'250'}}
+      t['zb']    = {'zb', {'250'}}
+      require('neoscroll.config').set_mappings(t)
+    end
+  },
   -- Filer
   {
     'nvim-tree/nvim-tree.lua',

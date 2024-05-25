22a142a6db0cff86ae26cbf5a89a8d03216b105e
izumin5210 <m@izum.in>
2024-05-25 10:52:59 +0900

    chore(neovim): tweak flash.nvim load timing

--- config/.config/nvim/init.lua
+++ config/.config/nvim/init.lua
@@ -490,6 +490,7 @@ require("lazy").setup({
   {
     "folke/flash.nvim",
     version = "*",
+    event = "VeryLazy",
     ---@type Flash.Config
     opts = {},
     -- stylua: ignore
@@ -499,12 +500,6 @@ require("lazy").setup({
       { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
       { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
       { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
-      { "f", mode = { "n" } },
-      { "F", mode = { "n" } },
-      { "t", mode = { "n" } },
-      { "T", mode = { "n" } },
-      { ",", mode = { "n" } },
-      { ";", mode = { "n" } },
     },
   },
   {

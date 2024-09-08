fb82c950ee24cf8fb6c183ff958979f856fd6dc1
izumin5210 <m@izum.in>
2024-09-08 12:52:34 +0900

    tweak

--- config/.config/nvim/lua/config/lazy.lua
+++ config/.config/nvim/lua/config/lazy.lua
@@ -14,4 +14,8 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("plugins")
+require("lazy").setup({
+  spec = {
+    { import = "plugins" }, -- load `plugins/init.lua`
+  },
+})

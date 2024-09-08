izumin5210/dotfiles

commit e3d9f720d4dcd407d61acae559dba13850f0f702

refactor(neovim): refactor plugin specs structure (#535)

config/.config/nvim/lua/plugins/editor/init.lua
@@ -1,0 +1,336 @@
+return {
+  {
+    "stevearc/aerial.nvim",
+    version = "*",
+    cond = not vim.g.vscode,
+    lazy = true,
+    config = true,
+  },
+  {
+    "nvim-tree/nvim-tree.lua",
+    version = "*",
+    cond = not vim.g.vscode,
+    dependencies = { "nvim-web-devicons" },
+    keys = function()
+      return require("plugins.editor.config.nvim-tree").keys()
+    end,
+    opts = function()
+      return require("plugins.editor.config.nvim-tree").opts()
+    end,
+  },
+  {
+    "nvim-telescope/telescope.nvim",
+    version = "*",
+    cond = not vim.g.vscode,
+    dependencies = {
+      "plenary.nvim",
+      {
+        "prochri/telescope-all-recent.nvim",
+        dependencies = { "kkharji/sqlite.lua" },
+        opts = {},
+      },
+      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+      "otavioschwanck/telescope-alternate.nvim",
+      "aerial.nvim",
+      "nvim-telescope/telescope-dap.nvim",
+    },
+    cmd = "Telescope",
+    keys = function()
+      ---@param name string
+      ---@return function
+      local function action(name)
+        return require("plugins.editor.config.telescope").actions[name]
+      end
+      return require("utils.keymap").lazy_keymap({
+        {
+          { "n", "<leader><leader>", action("find_files"), desc = "File: Go to ..." },
+          { "n", "<leader>gg", action("grep"), desc = "File: Grep" },
+          { "n", "<leader>gs", action("git_status"), desc = "File: Git Suatus" },
+          { "n", "<leader>gu", action("conflicted_files"), desc = "File: Git Unmerged Files" },
+          { "n", "<leader>gb", action("buffers"), desc = "File: Buffers" },
+          { "n", "<leader>ga", action("alternate_files"), desc = "File: Alternate" },
+          { "n", "<leader>gf", action("aerial"), desc = "LSP: Functions and Methods" },
+        },
+        common = { noremap = true },
+      })
+    end,
+    init = function()
+      require("plugins.editor.config.telescope").init()
+    end,
+    config = function()
+      require("plugins.editor.config.telescope").setup()
+    end,
+  },
+  {
+    "folke/which-key.nvim",
+    version = "*",
+    cond = not vim.g.vscode,
+    event = { "BufReadPost", "BufAdd", "BufNewFile" },
+    config = function()
+      local wk = require("which-key")
+      wk.setup({
+        plugins = {
+          presets = {
+            operators = false,
+          },
+        },
+      })
+      wk.add({
+        { "<leader>g", group = "+Go to File, Code or GitHub" },
+        { "<leader>t", group = "+Test" },
+        { "<leader>d", group = "+Debug" },
+        { "<leader>c", group = "+Comment" },
+      })
+    end,
+  },
+  {
+    "mfussenegger/nvim-dap",
+    cond = not vim.g.vscode,
+    dependencies = {
+      {
+        "leoluz/nvim-dap-go",
+        ft = "go",
+        config = require("plugins.editor.config.dap").setup_go,
+      },
+      {
+        "theHamsta/nvim-dap-virtual-text",
+        config = true,
+      },
+    },
+    keys = require("plugins.editor.config.dap").keys,
+    config = require("plugins.editor.config.dap").setup,
+  },
+  {
+    "nvim-neotest/neotest",
+    version = "*",
+    cond = not vim.g.vscode,
+    dependencies = {
+      { "nvim-neotest/nvim-nio", version = "*" },
+      "plenary.nvim",
+      "nvim-treesitter",
+      -- adapters
+      "nvim-neotest/neotest-go",
+      "nvim-neotest/neotest-jest",
+      "marilari88/neotest-vitest",
+    },
+    config = require("plugins.editor.config.neotest").setup,
+    keys = require("plugins.editor.config.neotest").keys,
+  },
+  {
+    "folke/todo-comments.nvim",
+    version = "*",
+    cond = not vim.g.vscode,
+    event = { "BufReadPost", "BufAdd", "BufNewFile" },
+    dependencies = { "plenary.nvim" },
+    opts = {
+      highlight = { after = "" },
+    },
+  },
+  {
+    "dinhhuy258/git.nvim",
+    cond = not vim.g.vscode,
+    keys = {
+      { "<Leader>go", desc = "Git: Open in GitHub" },
+      { "<Leader>gp", desc = "Git: Open Pull Request Page" },
+    },
+    config = true,
+  },
+  -- Buffers
+  {
+    "famiu/bufdelete.nvim",
+    cond = not vim.g.vscode,
+    lazy = true,
+    keys = {
+      {
+        "<C-q>",
+        function()
+          require("bufdelete").bufdelete(0, false)
+        end,
+        mode = "n",
+        noremap = true,
+        desc = "Buffer: Delete",
+      },
+    },
+  },
+  {
+    "chrisgrieser/nvim-early-retirement",
+    cond = not vim.g.vscode,
+    event = "VeryLazy",
+    opts = {
+      retirementAgeMins = 20,
+    },
+  },
+  -- Highlight
+  {
+    "petertriho/nvim-scrollbar",
+    cond = not vim.g.vscode,
+    event = { "BufReadPost", "BufAdd", "BufNewFile" },
+    opts = function()
+      local palette = require("utils.colors").palette
+      return {
+        marks = {
+          Search = { color_nr = "3", color = palette.yellow },
+          Error = { color_nr = "9", color = palette.red },
+          Warn = { color_nr = "11", color = palette.peach },
+        },
+        handlers = {
+          cursor = true,
+          diagnostic = true,
+          gitsigns = true,
+          handle = true,
+          search = true,
+        },
+      }
+    end,
+  },
+  {
+    "kevinhwang91/nvim-hlslens",
+    cond = not vim.g.vscode,
+    event = { "BufReadPost", "BufAdd", "BufNewFile" },
+    init = function()
+      require("utils.highlight").force_set_highlights("nvim-hlslens_hl", {
+        HlSearchLens = { link = "DiagnosticHint" },
+        HlSearchLensNear = { link = "DiagnosticInfo" },
+      })
+    end,
+    config = function()
+      require("scrollbar.handlers.search").setup({})
+    end,
+  },
+  {
+    "lewis6991/gitsigns.nvim",
+    version = "*",
+    cond = not vim.g.vscode,
+    event = { "BufReadPost", "BufAdd", "BufNewFile" },
+    init = function()
+      require("utils.highlight").force_set_highlights("gitsigns_hl", {
+        SignColumn = { ctermbg = "none", bg = "none" },
+        GitGutterAdd = { ctermbg = "none", bg = "none" },
+        GitGutterChange = { ctermbg = "none", bg = "none" },
+        GitGutterDelete = { ctermbg = "none", bg = "none" },
+      })
+    end,
+    config = function()
+      require("gitsigns").setup()
+      require("scrollbar.handlers.gitsigns").setup()
+    end,
+  },
+  {
+    "mvllow/modes.nvim",
+    version = "*",
+    cond = not vim.g.vscode,
+    event = { "CursorMoved", "CursorMovedI" },
+    opts = function()
+      local palette = require("utils.colors").palette
+      return {
+        colors = {
+          copy = palette.yellow,
+          delete = palette.red,
+          insert = palette.sky,
+          visual = palette.mauve,
+        },
+        line_opacity = {
+          copy = 0.4,
+          delete = 0.4,
+          insert = 0.4,
+          visual = 0.4,
+        },
+      }
+    end,
+  },
+  {
+    "shellRaining/hlchunk.nvim",
+    version = "*",
+    cond = not vim.g.vscode,
+    event = { "BufReadPost", "BufAdd", "BufNewFile" },
+    opts = function()
+      local palette = require("utils.colors").palette
+      local exclude_filetypes = {
+        aerial = true,
+        dashboard = true,
+        ["rip-substitute"] = true,
+        TelescopeResults = true,
+      }
+      return {
+        chunk = {
+          enable = true,
+          style = {
+            { fg = palette.sapphire },
+            { fg = palette.red },
+          },
+          delay = 0, -- disable animation
+          exclude_filetypes = exclude_filetypes,
+        },
+        indent = { enable = true, exclude_filetypes = exclude_filetypes },
+        line_num = { enable = false },
+        blank = { enable = false },
+      }
+    end,
+  },
+  {
+    "zbirenbaum/neodim",
+    cond = not vim.g.vscode,
+    event = "LspAttach",
+    config = true,
+  },
+  {
+    "brenoprata10/nvim-highlight-colors",
+    cond = not vim.g.vscode,
+    event = { "BufReadPost", "BufAdd", "BufNewFile" },
+    opts = {
+      render = "virtual",
+      virtual_symbol = " ■",
+      enable_named_colors = false,
+      enable_tailwind = true,
+      exclude_filetypes = {
+        "dashboard",
+        "lazy",
+      },
+    },
+  },
+  {
+    "RRethy/vim-illuminate",
+    cond = not vim.g.vscode,
+    event = { "CursorMoved", "CursorMovedI" },
+    init = function()
+      local palette = require("utils.colors").palette
+      require("utils.highlight").force_set_highlights("vim-illuminate_hl", {
+        IlluminatedWordText = { ctermbg = 238, bg = palette.surface1 },
+        IlluminatedWordRead = { ctermbg = 238, bg = palette.surface1 },
+        IlluminatedWordWrite = { ctermbg = 238, bg = palette.surface1 },
+      })
+    end,
+    config = function()
+      require("illuminate").configure()
+    end,
+  },
+  {
+    "ntpeters/vim-better-whitespace",
+    cond = not vim.g.vscode,
+    event = { "BufReadPost", "BufAdd", "BufNewFile" },
+    init = function()
+      local palette = require("utils.colors").palette
+      require("utils.highlight").force_set_highlights("vim-better-whitespace_hl", {
+        ExtraWhitespace = { bg = palette.red },
+      })
+    end,
+    config = function()
+      vim.g.better_whitespace_enabled = 1
+      vim.g.strip_whitespace_on_save = 1
+      vim.g.strip_whitespace_confirm = 0
+      vim.g.better_whitespace_filetypes_blacklist = {
+        "dashboard",
+        "lazy",
+        -- default values
+        "diff",
+        "git",
+        "gitcommit",
+        "unite",
+        "qf",
+        "help",
+        "markdown",
+        "fugitive",
+      }
+    end,
+  },
+}

config/.config/nvim/lua/plugins/treesitter.lua
@@ -1,0 +1,133 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    version = "*",
+    cond = not vim.g.vscode,
+    event = { "BufReadPre" },
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = {
+          "bash",
+          "css",
+          "cue",
+          -- 'diff',
+          "dockerfile",
+          "git_config",
+          "git_rebase",
+          "gitattributes",
+          "gitcommit",
+          "gitignore",
+          "go",
+          "gomod",
+          "gosum",
+          "gowork",
+          "graphql",
+          "hcl",
+          "html",
+          "javascript",
+          "jq",
+          "jsdoc",
+          "json",
+          "json5",
+          "jsonnet",
+          "lua",
+          "make",
+          "markdown",
+          "markdown_inline", -- required by lspsaga.nvim
+          "prisma",
+          "proto",
+          "python",
+          "regex",
+          "ruby",
+          "rust",
+          "scss",
+          "sql",
+          "starlark",
+          "toml",
+          "tsx",
+          "typescript",
+          "vim",
+          "vue",
+          "yaml",
+        },
+        -- indent = {
+        --   enable = true,
+        -- },
+        highlight = {
+          enable = true,
+          additional_vim_regex_highlighting = false,
+        },
+        context_commentstring = {
+          enable_autocmd = false,
+        },
+        matchup = {
+          enable = true,
+        },
+      })
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+    cond = not vim.g.vscode,
+    event = { "BufReadPre" },
+    dependencies = { "nvim-treesitter" },
+    init = function()
+      local palette = require("utils.colors").palette
+
+      require("utils.highlight").force_set_highlights("treesitter-context_hl", {
+        TreesitterContext = { bg = palette.surface1, blend = 10 },
+      })
+    end,
+    opts = {
+      max_lines = 4,
+    },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects", -- required by nvim-surround
+    cond = not vim.g.vscode,
+    event = { "BufReadPre" },
+    dependencies = { "nvim-treesitter" },
+  },
+  {
+    "haringsrob/nvim_context_vt",
+    cond = not vim.g.vscode,
+    event = { "BufReadPre" },
+    dependencies = { "nvim-treesitter" },
+    init = function()
+      require("utils.highlight").force_set_highlights("context_vt_hl", {
+        ContextVt = { link = "DiagnosticHint" },
+      })
+    end,
+    opts = {
+      min_rows = 3,
+    },
+  },
+  {
+    "andymass/vim-matchup",
+    version = "*",
+    cond = not vim.g.vscode,
+    event = { "BufReadPre" },
+    dependencies = { "nvim-treesitter" },
+  },
+  {
+    "windwp/nvim-ts-autotag",
+    cond = not vim.g.vscode,
+    event = { "BufReadPre" },
+    dependencies = { "nvim-treesitter" },
+    opts = {
+      opts = {
+        enable_close = true,
+        enable_rename = true,
+        enable_close_on_slash = true,
+      },
+    },
+  },
+  {
+    "folke/ts-comments.nvim",
+    cond = not vim.g.vscode,
+    version = "*",
+    event = { "BufReadPost", "BufAdd", "BufNewFile" },
+    dependencies = { "nvim-treesitter" },
+    opts = {},
+  },
+}

config/.config/nvim/lua/plugins/ui/init.lua
@@ -1,0 +1,127 @@
+return {
+  {
+    "akinsho/bufferline.nvim",
+    cond = not vim.g.vscode,
+    event = { "BufReadPost", "BufWritePost", "BufNewFile" },
+    version = "*",
+    dependencies = { "nvim-web-devicons" },
+    opts = function()
+      return require("plugins.ui.config.bufferline").opts()
+    end,
+  },
+  {
+    "folke/noice.nvim",
+    version = "*",
+    cond = not vim.g.vscode,
+    event = { "BufReadPost", "BufWritePost", "BufNewFile" },
+    dependencies = {
+      "nui.nvim",
+      {
+        "rcarriga/nvim-notify",
+        version = "*",
+        init = function()
+          local palette = require("utils.colors").palette
+          require("utils.highlight").set_highlights("nvim-notify_hl", {
+            NotifyBackground = { bg = palette.base },
+            NotifyERRORBorder = { bg = palette.base },
+            NotifyERRORBody = { bg = palette.base },
+            NotifyWARNBorder = { bg = palette.base },
+            NotifyWARNBody = { bg = palette.base },
+            NotifyINFOBorder = { bg = palette.base },
+            NotifyINFOBody = { bg = palette.base },
+            NotifyDEBUGBorder = { bg = palette.base },
+            NotifyDEBUGBody = { bg = palette.base },
+            NotifyTRACEBorder = { bg = palette.base },
+            NotifyTRACEBody = { bg = palette.base },
+          })
+        end,
+        opts = {
+          render = "wrapped-compact",
+          stages = "static",
+          timeout = 3000,
+          max_height = function()
+            return math.floor(vim.o.lines * 0.75)
+          end,
+          max_width = function()
+            return math.floor(vim.o.columns * 0.50)
+          end,
+        },
+      },
+    },
+    opts = {
+      lsp = {
+        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+          ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+        },
+        signature = { enabled = true },
+        progress = { enabled = true },
+        hover = { enabled = false }, -- use lspsaga
+      },
+      -- you can enable a preset for easier configuration
+      presets = {
+        bottom_search = true, -- use a classic bottom cmdline for search
+        command_palette = true, -- position the cmdline and popupmenu together
+        long_message_to_split = true, -- long messages will be sent to a split
+        inc_rename = false, -- enables an input dialog for inc-rename.nvim
+        lsp_doc_border = false, -- add a border to hover docs and signature help
+      },
+      routes = {
+        {
+          filter = { event = "notify", find = "No information available" },
+          opts = { skip = true },
+        },
+      },
+      messages = {
+        view_search = false, -- use hlslens
+      },
+      views = {
+        cmdline_popup = {
+          border = { style = "none", padding = { 1, 3 } },
+          win_options = {
+            winhighlight = { NormalFloat = "NoiceCmdlinePopupNormal", FloatBorder = "NoiceCmdlinePopupBorder" },
+          },
+        },
+        cmdline_popupmenu = {
+          border = { style = "none", padding = { 1, 3 } },
+          win_options = {
+            winhighlight = { NormalFloat = "NoiceCmdlinePopupmenuNormal", FloatBorder = "NoiceCmdlinePopupmenuBorder" },
+          },
+        },
+      },
+    },
+    init = function()
+      local palette = require("utils.colors").palette
+
+      require("utils.highlight").set_highlights("noice_hl", {
+        NoiceCmdlinePopupNormal = { link = "NormalFloat" },
+        NoiceCmdlinePopupBorder = { link = "FloatBorder" },
+        NoiceCmdlinePopupmenuNormal = { fg = palette.text, bg = palette.mantle },
+        NoiceCmdlinePopupmenuBorder = { fg = palette.text, bg = palette.mantle },
+      })
+    end,
+  },
+  {
+    "nvimdev/dashboard-nvim",
+    lazy = false,
+    cond = not vim.g.vscode,
+    config = function()
+      require("dashboard").setup({
+        theme = "hyper",
+        config = {
+          -- stylua: ignore
+          shortcut = {
+            { action = 'lua require("persistence").load()', desc = " Restore Session", icon = " ", key = "s" },
+          },
+          project = { enable = false },
+          mru = { cwd_only = true },
+          header = {},
+          footer = {},
+        },
+      })
+    end,
+    dependencies = { "nvim-web-devicons" },
+  },
+}

config/.config/nvim/lua/plugins/edit/init.lua
@@ -1,0 +1,153 @@
+return {
+  {
+    "hrsh7th/nvim-cmp",
+    cond = not vim.g.vscode,
+    event = "InsertEnter",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-vsnip",
+      "hrsh7th/vim-vsnip",
+      {
+        "zbirenbaum/copilot-cmp",
+        dependencies = {
+          "zbirenbaum/copilot.lua",
+          opts = {
+            suggestion = { enabled = false },
+            panel = { enabled = false },
+          },
+        },
+        opts = {
+          method = "getCompletionsCycling",
+        },
+      },
+      {
+        "onsails/lspkind.nvim",
+        config = function()
+          require("lspkind").init({
+            symbol_map = {
+              Copilot = "",
+            },
+          })
+        end,
+      },
+    },
+    keys = require("plugins.edit.config.nvim-cmp").keys,
+    init = require("plugins.edit.config.nvim-cmp").init,
+    config = require("plugins.edit.config.nvim-cmp").setup,
+  },
+  {
+    "windwp/nvim-autopairs",
+    cond = not vim.g.vscode,
+    event = "InsertEnter",
+    config = true,
+  },
+  {
+    "folke/flash.nvim",
+    version = "*",
+    event = { "BufReadPost", "BufAdd", "BufNewFile" },
+    ---@type Flash.Config
+    opts = {},
+    -- stylua: ignore
+    keys = {
+      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+      { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+      { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+      { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+      { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    },
+  },
+  {
+    "numToStr/Comment.nvim",
+    cond = not vim.g.vscode,
+    keys = { { "<Leader>/", mode = { "n", "v" } } },
+    opts = {
+      toggler = {
+        line = "<Leader>/",
+      },
+      opleader = {
+        line = "<Leader>/",
+      },
+    },
+  },
+  {
+    "danymat/neogen",
+    version = "*",
+    cond = not vim.g.vscode,
+    keys = {
+      {
+        "<Leader>cd",
+        function()
+          require("neogen").generate()
+        end,
+        mode = "n",
+        desc = "Generate doc comment",
+      },
+    },
+    dependencies = { "nvim-treesitter" },
+    opts = {
+      snippet_engine = "vsnip",
+    },
+  },
+  {
+    "kylechui/nvim-surround",
+    version = "*",
+    event = { "CursorHold", "CursorHoldI" },
+    config = true,
+  },
+  {
+    "chrisgrieser/nvim-rip-substitute",
+    cond = not vim.g.vscode,
+    cmd = "RipSubstitute",
+    keys = {
+      {
+        "<leader>fs",
+        function()
+          require("rip-substitute").sub()
+        end,
+        mode = { "n", "x" },
+        desc = " rip substitute",
+      },
+    },
+    opts = {
+      popupWin = {
+        border = { " ", " ", " ", " ", " ", " ", " ", " " },
+      },
+      prefill = {
+        normal = false,
+      },
+    },
+  },
+  {
+    "monaqa/dial.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    keys = require("plugins.edit.config.dial").keys,
+    config = require("plugins.edit.config.dial").setup,
+  },
+  {
+    "rapan931/lasterisk.nvim",
+    keys = {
+      {
+        "*",
+        function()
+          require("lasterisk").search()
+          if package.loaded["hlslens"] then
+            require("hlslens").start()
+          end
+        end,
+        mode = "n",
+      },
+      {
+        "g*",
+        function()
+          require("lasterisk").search({ is_whole = false })
+          if package.loaded["hlslens"] then
+            require("hlslens").start()
+          end
+        end,
+        mode = { "n", "x" },
+      },
+    },
+  },
+}

config/.config/nvim/lua/plugins/init.lua
@@ -1,0 +1,130 @@
+return {
+  -- Libraries
+  {
+    "nvim-tree/nvim-web-devicons",
+    cond = not vim.g.vscode,
+    lazy = true,
+    config = true,
+  },
+  {
+    "mortepau/codicons.nvim", -- required by config function for nvim-dap
+    cond = not vim.g.vscode,
+    lazy = true,
+  },
+  {
+    "nvim-lua/plenary.nvim",
+    cond = not vim.g.vscode,
+    lazy = true,
+    version = "*",
+  },
+  {
+    "MunifTanjim/nui.nvim",
+    version = "*",
+    cond = not vim.g.vscode,
+    lazy = true,
+  },
+  {
+    "tpope/vim-repeat", -- required by leap.nvim
+    event = { "BufReadPost", "BufAdd", "BufNewFile" },
+  },
+  -- Appearance
+  {
+    "catppuccin/nvim",
+    version = "*",
+    name = "catppuccin",
+    cond = not vim.g.vscode,
+    priority = 1000,
+    opts = {
+      flavour = "frappe",
+      transparent_background = true,
+      integrations = {
+        aerial = true,
+        lsp_saga = true,
+        mason = true,
+        neotest = true,
+        noice = true,
+        notify = true,
+        which_key = true,
+      },
+    },
+  },
+  -- misc
+  {
+    "folke/persistence.nvim",
+    version = "*",
+    cond = not vim.g.vscode,
+    event = "BufReadPre", -- this will only start session saving when an actual file was opened
+    opts = {},
+  },
+  {
+    "alexghergh/nvim-tmux-navigation",
+    cond = not vim.g.vscode,
+    keys = {
+      {
+        "<C-w>h",
+        function()
+          require("nvim-tmux-navigation").NvimTmuxNavigateLeft()
+        end,
+        mode = "n",
+        noremap = true,
+      },
+      {
+        "<C-w>j",
+        function()
+          require("nvim-tmux-navigation").NvimTmuxNavigateDown()
+        end,
+        mode = "n",
+        noremap = true,
+      },
+      {
+        "<C-w>k",
+        function()
+          require("nvim-tmux-navigation").NvimTmuxNavigateUp()
+        end,
+        mode = "n",
+        noremap = true,
+      },
+      {
+        "<C-w>l",
+        function()
+          require("nvim-tmux-navigation").NvimTmuxNavigateRight()
+        end,
+        mode = "n",
+        noremap = true,
+      },
+      {
+        "<C-w>\\",
+        function()
+          require("nvim-tmux-navigation").NvimTmuxNavigateLastActive()
+        end,
+        mode = "n",
+        noremap = true,
+      },
+      {
+        "<C-w>Space",
+        function()
+          require("nvim-tmux-navigation").NvimTmuxNavigateNext()
+        end,
+        mode = "n",
+        noremap = true,
+      },
+    },
+    opts = {
+      disable_when_zoomed = true,
+    },
+  },
+  {
+    "folke/lazydev.nvim",
+    version = "*",
+    cond = not vim.g.vscode,
+    ft = "lua",
+    dependencies = {
+      { "Bilal2453/luvit-meta", lazy = true }, -- optional `vim.uv` typings
+    },
+    opts = {
+      library = {
+        { path = "luvit-meta/library", words = { "vim%.uv" } },
+      },
+    },
+  },
+}

config/.config/nvim/lua/plugins/lang/init.lua
@@ -1,0 +1,33 @@
+return {
+  -- lang
+  {
+    "vuki656/package-info.nvim",
+    cond = not vim.g.vscode,
+    dependencies = { "nui.nvim" },
+    event = { "BufEnter package.json" },
+    init = function()
+      require("utils.highlight").force_set_highlights("package-info_hl", {
+        PackageInfoOutdatedVersion = { link = "DiagnosticHint" },
+        PackageInfoUpToDateVersion = { link = "DiagnosticHint" },
+      })
+    end,
+    config = function()
+      require("package-info").setup({
+        autostart = true,
+        hide_up_to_date = true,
+        hide_unstable_version = true,
+      })
+      require("package-info").show()
+    end,
+  },
+  -- Syntax Highlighting
+  {
+    "jxnblk/vim-mdx-js",
+    cond = not vim.g.vscode,
+  },
+  {
+    "direnv/direnv.vim",
+    cond = not vim.g.vscode,
+    ft = "direnv",
+  },
+}

commit 5f2ffd3eb5ca734a7cbc9d6651ec82bba3a78d86

fix(neovim): load some plugins on `VeryLazy` (#536)
broken in #535

config/.config/nvim/lua/plugins/editor/init.lua
@@ -120,7 +120,7 @@ return {
     "folke/todo-comments.nvim",
     version = "*",
     cond = not vim.g.vscode,
-    event = { "BufReadPost", "BufAdd", "BufNewFile" },
+    event = { "VeryLazy" },
     dependencies = { "plenary.nvim" },
     opts = {
       highlight = { after = "" },
@@ -307,7 +307,7 @@ return {
   {
     "ntpeters/vim-better-whitespace",
     cond = not vim.g.vscode,
-    event = { "BufReadPost", "BufAdd", "BufNewFile" },
+    event = { "VeryLazy" },
     init = function()
       local palette = require("utils.colors").palette
       require("utils.highlight").force_set_highlights("vim-better-whitespace_hl", {

config/.config/nvim/lua/plugins/treesitter.lua
@@ -126,7 +126,7 @@ return {
     "folke/ts-comments.nvim",
     cond = not vim.g.vscode,
     version = "*",
-    event = { "BufReadPost", "BufAdd", "BufNewFile" },
+    event = { "VeryLazy" },
     dependencies = { "nvim-treesitter" },
     opts = {},
   },

config/.config/nvim/lua/plugins/ui/init.lua
@@ -2,7 +2,7 @@ return {
   {
     "akinsho/bufferline.nvim",
     cond = not vim.g.vscode,
-    event = { "BufReadPost", "BufWritePost", "BufNewFile" },
+    event = { "VeryLazy" },
     version = "*",
     dependencies = { "nvim-web-devicons" },
     opts = function()
@@ -13,7 +13,7 @@ return {
     "folke/noice.nvim",
     version = "*",
     cond = not vim.g.vscode,
-    event = { "BufReadPost", "BufWritePost", "BufNewFile" },
+    event = { "VeryLazy" },
     dependencies = {
       "nui.nvim",
       {
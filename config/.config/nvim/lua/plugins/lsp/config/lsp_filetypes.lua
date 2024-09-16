9efed91cfba27b51bbadd945798bd1c5d1190dd3
Masayuki Izumi <m@izum.in>
2024-09-16 15:53:06 +0900

    chore(neovim): enable ESLint on graphql files (#549)

--- config/.config/nvim/lua/plugins/lsp/config/lsp_filetypes.lua
+++ config/.config/nvim/lua/plugins/lsp/config/lsp_filetypes.lua
@@ -1,5 +1,12 @@
+-- stylua: ignore
+local defaults = {
+  eslint = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx", "vue", "svelte", "astro" },
+  graphql = { "graphql", "typescriptreact", "javascriptreact" },
+}
+
 local lsp_filetypes = {
-  graphql = { "graphql", "typescriptreact", "javascriptreact", "typescript", "javascript", "vue" },
+  graphql = vim.list_extend({ "typescript", "javascript", "vue" }, defaults.graphql),
+  eslint = vim.list_extend({ "graphql" }, defaults.eslint),
 }
 
 return lsp_filetypes

77717115d311d0b42a8ae9b3bda724c8282e163c
izumin5210 <m@izum.in>
2024-11-25 10:07:43 +0900

    chore(neovim): support cva and shadcn/ui

--- config/.config/nvim/lua/plugins/lsp/config/lsp_settings.lua
+++ config/.config/nvim/lua/plugins/lsp/config/lsp_settings.lua
@@ -44,6 +44,14 @@ local lsp_settings = {
           -- https://github.com/paolotiu/tailwind-intellisense-regex-list
           { "clsx\\(([^)]*)\\)", "(?:'|\"|`)([^']*)(?:'|\"|`)" },
           "(?:enter|leave)(?:From|To)?=\\s*(?:\"|')([^(?:\"|')]*)",
+
+          -- https://cva.style/docs/getting-started/installation#intellisense
+          { "cva\\(([^)]*)\\)", "[\"'`]([^\"'`]*).*?[\"'`]" },
+          { "cn\\(([^)]*)\\)", "(?:'|\"|`)([^']*)(?:'|\"|`)" },
+
+          -- shadcn/ui's `cn` helper
+          -- https://github.com/shadcn-ui/ui/blob/shadcn-ui%400.9.3/packages/shadcn/src/utils/templates.ts#L1-L7
+          { "cx\\(([^)]*)\\)", "(?:'|\"|`)([^']*)(?:'|\"|`)" },
         },
       },
     },

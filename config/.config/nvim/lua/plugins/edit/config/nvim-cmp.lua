6d7110aa8b77508a5f1281ddaa5450e03908ac25
izumin5210 <m@izum.in>
2024-12-07 11:33:35 +0900

    feat(neovim): add LuaSnip

--- config/.config/nvim/lua/plugins/edit/config/nvim-cmp.lua
+++ config/.config/nvim/lua/plugins/edit/config/nvim-cmp.lua
@@ -60,40 +60,39 @@ function M.init()
   })
 end
 
--- https://github.com/zbirenbaum/copilot-cmp/tree/b6e5286?tab=readme-ov-file#tab-completion-configuration-highly-recommended
-local has_words_before = function()
-  if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
-    return false
-  end
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
-end
-
 function M.setup()
   local cmp = require("cmp")
+  local luasnip = require("luasnip")
 
   cmp.setup({
     completion = {
       completeopt = "menu,menuone,noinsert",
+    },
+    snippet = {
+      expand = function(args)
+        luasnip.lsp_expand(args.body)
+      end,
     },
     mapping = cmp.mapping.preset.insert({
       ["<C-b>"] = cmp.mapping.scroll_docs(-4),
       ["<C-f>"] = cmp.mapping.scroll_docs(4),
       ["<C-Space>"] = cmp.mapping.complete(),
       ["<C-e>"] = cmp.mapping.abort(),
-      -- https://github.com/zbirenbaum/copilot-cmp/tree/b6e5286?tab=readme-ov-file#tab-completion-configuration-highly-recommended
-      ["<Tab>"] = vim.schedule_wrap(function(fallback)
-        if cmp.visible() and has_words_before() then
-          cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+      ["<CR>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          if luasnip.expandable() then
+            luasnip.expand()
+          else
+            cmp.confirm({ select = true })
+          end
         else
           fallback()
         end
       end),
-      -- ["<CR>"] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false }),
-      ["<CR>"] = cmp.mapping.confirm({ select = true }),
     }),
     sources = cmp.config.sources({
       { name = "copilot", group_index = 1, priority = 1000 },
+      { name = "luasnip", group_index = 1, priority = 100 },
       { name = "nvim_lsp", group_index = 1, priority = 100 },
       { name = "go_pkgs", group_index = 1, priority = 100 },
       { name = "path", group_index = 1, priority = 10 },

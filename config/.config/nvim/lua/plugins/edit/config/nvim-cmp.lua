9becc8411d551a7fe64c00500a742c3d693bdc4f
izumin5210 <m@izum.in>
2024-12-07 10:29:44 +0900

    chore(neovim): tweak nvim-cmp config

--- config/.config/nvim/lua/plugins/edit/config/nvim-cmp.lua
+++ config/.config/nvim/lua/plugins/edit/config/nvim-cmp.lua
@@ -60,31 +60,44 @@ function M.init()
   })
 end
 
+-- https://github.com/zbirenbaum/copilot-cmp/tree/b6e5286?tab=readme-ov-file#tab-completion-configuration-highly-recommended
+local has_words_before = function()
+  if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
+    return false
+  end
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
+end
+
 function M.setup()
   local cmp = require("cmp")
 
   cmp.setup({
-    snippet = {
-      expand = function(args)
-        vim.fn["vsnip#anonymous"](args.body)
-      end,
+    completion = {
+      completeopt = "menu,menuone,noinsert",
     },
     mapping = cmp.mapping.preset.insert({
       ["<C-b>"] = cmp.mapping.scroll_docs(-4),
       ["<C-f>"] = cmp.mapping.scroll_docs(4),
       ["<C-Space>"] = cmp.mapping.complete(),
       ["<C-e>"] = cmp.mapping.abort(),
-      ["<CR>"] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false }), -- zbirenbaum/copilot-cmp
+      -- https://github.com/zbirenbaum/copilot-cmp/tree/b6e5286?tab=readme-ov-file#tab-completion-configuration-highly-recommended
+      ["<Tab>"] = vim.schedule_wrap(function(fallback)
+        if cmp.visible() and has_words_before() then
+          cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+        else
+          fallback()
+        end
+      end),
+      -- ["<CR>"] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false }),
+      ["<CR>"] = cmp.mapping.confirm({ select = true }),
     }),
     sources = cmp.config.sources({
-      { name = "nvim_lsp" },
-      { name = "copilot" },
-      { name = "path" },
-    }, {
-      { name = "buffer" },
-    }, {
-      name = "lazydev",
-      group_index = 0,
+      { name = "copilot", group_index = 1, priority = 1000 },
+      { name = "nvim_lsp", group_index = 1, priority = 100 },
+      { name = "path", group_index = 1, priority = 10 },
+      { name = "buffer", group_index = 1, priority = 1 },
+      { name = "lazydev", group_index = 0 },
     }),
     -- https://github.com/hrsh7th/nvim-cmp/wiki/Menu-Appearance#how-to-get-types-on-the-left-and-offset-the-menu
     window = {

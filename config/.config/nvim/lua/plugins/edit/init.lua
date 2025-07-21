309f8272ac47d01c513c764ab84ff46d5e3d1bcf
Masayuki Izumi <m@izum.in>
2025-07-21 11:26:07 +0900

    chore: add copilot-lsp, playwright mcp, o3-search mcp (#1112)

--- config/.config/nvim/lua/plugins/edit/init.lua
+++ config/.config/nvim/lua/plugins/edit/init.lua
@@ -54,6 +54,21 @@ return {
     config = require("plugins.edit.config.nvim-cmp").setup,
   },
   {
+    "copilotlsp-nvim/copilot-lsp",
+    dependencies = { "dotfiles-node-tools" },
+    init = function()
+      vim.g.copilot_nes_debounce = 500
+      vim.lsp.enable("copilot_ls")
+      vim.keymap.set("n", "<tab>", function()
+        -- Try to jump to the start of the suggestion edit.
+        -- If already at the start, then apply the pending suggestion and jump to the end of the edit.
+        local _ = require("copilot-lsp.nes").walk_cursor_start_edit()
+          or (require("copilot-lsp.nes").apply_pending_nes() and require("copilot-lsp.nes").walk_cursor_end_edit())
+      end)
+    end,
+    opts = {},
+  },
+  {
     "windwp/nvim-autopairs",
     cond = not vim.g.vscode,
     event = "InsertEnter",

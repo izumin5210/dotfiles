1dec61a773a185553e3d33e5743a31f64784613c
izumin5210 <m@izum.in>
2025-07-27 18:53:43 +0900

    feat: add AeroSpace and JankyBorders

--- darwin-configuration.nix
+++ darwin-configuration.nix
@@ -45,39 +45,45 @@
     touchIdAuth = true;
   };
 
+  services.jankyborders = {
+    enable = true;
+    active_color = "0xffe1e3e4";
+    inactive_color = "0xff494d64";
+    width = 3.0;
+  };
+
   homebrew = {
     enable = true;
-    casks =
-      [
-        "alacritty"
-        "chatgpt"
-        "claude"
-        "dropbox"
-        "ghostty"
-        "google-japanese-ime"
-        "hammerspoon"
-        "jordanbaird-ice"
-        "karabiner-elements"
-        "obsidian"
-        "raycast"
-        "setapp"
-        "spotify"
-        "visual-studio-code"
-      ]
-      ++ (lib.optionals (!isWorkMac) [
-        "1password"
-        "google-chrome"
-      ]);
-    masApps =
-      {
-        # ...
-      }
-      // (lib.optionalAttrs (!isWorkMac) {
-        "Slack for Desktop" = 803453959;
-      })
-      // (lib.optionalAttrs (isWorkMac) {
-        "Twingate" = 1501592214;
-      });
+    casks = [
+      "alacritty"
+      "chatgpt"
+      "claude"
+      "dropbox"
+      "ghostty"
+      "google-japanese-ime"
+      "hammerspoon"
+      "jordanbaird-ice"
+      "karabiner-elements"
+      "obsidian"
+      "raycast"
+      "setapp"
+      "spotify"
+      "visual-studio-code"
+      "nikitabobko/tap/aerospace"
+    ]
+    ++ (lib.optionals (!isWorkMac) [
+      "1password"
+      "google-chrome"
+    ]);
+    masApps = {
+      # ...
+    }
+    // (lib.optionalAttrs (!isWorkMac) {
+      "Slack for Desktop" = 803453959;
+    })
+    // (lib.optionalAttrs (isWorkMac) {
+      "Twingate" = 1501592214;
+    });
   };
 
   nix.enable = false;

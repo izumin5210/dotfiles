63d17e7bc8273df43c1661ab152575787d0b8e17
izumin5210 <m@izum.in>
2025-08-31 15:24:43 +0900

    enable render-markdown only in obsidian vaults

--- config/.config/nvim/lua/plugins/obsidian/init.lua
+++ config/.config/nvim/lua/plugins/obsidian/init.lua
@@ -9,6 +9,41 @@ return {
     "obsidian-nvim/obsidian.nvim",
     version = "*",
     ft = "markdown",
+    init = function()
+      local grp = vim.api.nvim_create_augroup("obsidian_vault_markdown_policy", { clear = true })
+      vim.api.nvim_create_autocmd({
+        "BufReadPost",
+        "BufNewFile",
+        "BufFilePost",
+        "BufEnter",
+        "FileType",
+      }, {
+        group = grp,
+        callback = function(args)
+          local bufnr = args.buf
+          if not vim.api.nvim_buf_is_valid(bufnr) then
+            return
+          end
+          if vim.bo[bufnr].filetype ~= "markdown" then
+            return
+          end
+
+          local path = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(bufnr), ":p")
+          local enable = string.match(path, "^" .. vim.pesc(Obsidian.dir.filename) .. "/")
+
+          -- enable render-markdown and cnoceallevel only if in Obsidian vaults
+          vim.api.nvim_buf_call(bufnr, function()
+            if enable then
+              vim.opt_local.conceallevel = 2
+              require("render-markdown").buf_enable()
+            else
+              vim.opt_local.conceallevel = 0
+              require("render-markdown").buf_disable()
+            end
+          end)
+        end,
+      })
+    end,
     ---@type obsidian.config
     opts = {
       legacy_commands = false,
@@ -50,6 +85,15 @@ return {
           Obsidian.picker.grep_notes(Obsidian.picker, { callback = open_in_scratch })
         end,
       },
+    },
+  },
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    dependencies = { "nvim-treesitter", "nvim-web-devicons" },
+    lazy = true,
+    ---@type render.md.UserConfig
+    opts = {
+      completions = { blink = { enabled = true } },
     },
   },
 }

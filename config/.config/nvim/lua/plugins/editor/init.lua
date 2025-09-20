4638e2d6fa418b3b917ba2b20c4b4a61e25c6d4a
Masayuki Izumi <m@izum.in>
2025-09-20 23:20:19 +0900

    chore(neovim): add yazi.nvim instead of nvim-tree.lua (#1390)

--- config/.config/nvim/lua/plugins/editor/init.lua
+++ config/.config/nvim/lua/plugins/editor/init.lua
@@ -7,15 +7,32 @@ return {
     config = true,
   },
   {
-    "nvim-tree/nvim-tree.lua",
+    "mikavilpas/yazi.nvim",
     version = "*",
-    cond = not vim.g.vscode,
-    dependencies = { "nvim-web-devicons" },
-    keys = function()
-      return require("plugins.editor.config.nvim-tree").keys()
-    end,
-    opts = function()
-      return require("plugins.editor.config.nvim-tree").opts()
+    lazy = true,
+    dependencies = {
+      { "plenary.nvim", lazy = true },
+    },
+    keys = {
+      { "<leader>fe", mode = { "n" }, "<cmd>Yazi<cr>", desc = "Explorer (current file)" },
+      { "<leader>fE", mode = { "n" }, "<cmd>Yazi cwd<cr>", desc = "Explorer (cwd)" },
+    },
+    ---@type YaziConfig | {}
+    opts = {
+      open_for_directories = false,
+      keymaps = {
+        show_help = "<f1>",
+      },
+      yazi_floating_window_border = { " ", " ", " ", " ", " ", " ", " ", " " },
+    },
+    init = function()
+      vim.g.loaded_netrwPlugin = 1
+
+      local palette = require("utils.colors").palette
+      require("utils.highlight").set_highlights("yazi_hl", {
+        YaziFloat = { bg = palette.mantle },
+        YaziFloatBorder = { bg = palette.mantle },
+      })
     end,
   },
   {

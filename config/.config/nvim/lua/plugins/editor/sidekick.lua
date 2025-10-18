82e7f179d78e992e3642c360f59d9cf8f68c1ea0
izumin5210 <m@izum.in>
2025-10-18 10:16:06 +0900

    chore(neovim): bump plugins and migrate sidekick 2.0 config

--- config/.config/nvim/lua/plugins/editor/sidekick.lua
+++ config/.config/nvim/lua/plugins/editor/sidekick.lua
@@ -1,12 +1,12 @@
 return {
   "folke/sidekick.nvim",
   version = "*",
+  ---@class sidekick.Config
   opts = {
     cli = {
       mux = { backend = "tmux", enabled = false },
     },
   },
-  -- stylua: ignore
   keys = {
     {
       "<tab>",
@@ -21,44 +21,64 @@ return {
     },
     {
       "<leader>aa",
-      function() require("sidekick.cli").toggle() end,
+      function()
+        require("sidekick.cli").toggle()
+      end,
       desc = "Sidekick Toggle CLI",
     },
     {
       "<leader>as",
-      function() require("sidekick.cli").select() end,
+      function()
+        require("sidekick.cli").select()
+      end,
       -- Or to select only installed tools:
       -- require("sidekick.cli").select({ filter = { installed = true } })
       desc = "Select CLI",
     },
     {
+      "<leader>ad",
+      function()
+        require("sidekick.cli").close()
+      end,
+      desc = "Detach a CLI Session",
+    },
+    {
       "<leader>at",
-      function() require("sidekick.cli").send({ msg = "{this}" }) end,
+      function()
+        require("sidekick.cli").send({ msg = "{this}" })
+      end,
       mode = { "x", "n" },
       desc = "Send This",
     },
     {
+      "<leader>af",
+      function()
+        require("sidekick.cli").send({ msg = "{file}" })
+      end,
+      desc = "Send File",
+    },
+    {
       "<leader>av",
-      function() require("sidekick.cli").send({ msg = "{selection}" }) end,
+      function()
+        require("sidekick.cli").send({ msg = "{selection}" })
+      end,
       mode = { "x" },
       desc = "Send Visual Selection",
     },
     {
       "<leader>ap",
-      function() require("sidekick.cli").prompt() end,
+      function()
+        require("sidekick.cli").prompt()
+      end,
       mode = { "n", "x" },
       desc = "Sidekick Select Prompt",
-    },
-    {
-      "<c-.>",
-      function() require("sidekick.cli").focus() end,
-      mode = { "n", "x", "i", "t" },
-      desc = "Sidekick Switch Focus",
     },
     -- Example of a keybinding to open Claude directly
     {
       "<leader>ac",
-      function() require("sidekick.cli").toggle({ name = "claude", focus = true }) end,
+      function()
+        require("sidekick.cli").toggle({ name = "claude", focus = true })
+      end,
       desc = "Sidekick Toggle Claude",
     },
   },

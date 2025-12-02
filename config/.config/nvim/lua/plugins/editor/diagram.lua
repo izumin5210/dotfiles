7f071d2a49f240cc10cff2d7cb92f404544ada5b
izumin5210 <m@izum.in>
2025-12-02 11:39:32 +0900

    disable automatic rendering

--- config/.config/nvim/lua/plugins/editor/diagram.lua
+++ config/.config/nvim/lua/plugins/editor/diagram.lua
@@ -4,10 +4,27 @@ return {
     { "3rd/image.nvim", opts = {} },
   },
   opts = {
+    -- Disable automatic rendering for manual-only workflow
     events = {
-      render_buffer = { "InsertLeave", "BufWinEnter", "TextChanged" },
+      render_buffer = {}, -- Empty = no automatic rendering
       clear_buffer = { "BufLeave" },
     },
-    renderer_options = {},
+    renderer_options = {
+      mermaid = {
+        theme = "dark",
+        scale = 2,
+      },
+    },
+  },
+  keys = {
+    {
+      "<CR>",
+      function()
+        require("diagram").show_diagram_hover()
+      end,
+      mode = "n",
+      ft = { "markdown" },
+      desc = "Show diagram in new tab",
+    },
   },
 }
